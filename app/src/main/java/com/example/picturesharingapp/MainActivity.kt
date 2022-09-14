4bab3cc03a29356e51973da4caef7b0fc098dc01
Ahnaf <ahnaf.azad2@gmail.com>
2022-09-14 14:53:04 +0100

    version 1 final

--- app/src/main/java/com/example/picturesharingapp/MainActivity.kt
+++ app/src/main/java/com/example/picturesharingapp/MainActivity.kt
@@ -26,22 +26,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //calling function to load a meme as soon as app is started
         loadMeme()
     }
 
+    //this function uses Glide nad Volley to create API request and convert to image
     private fun loadMeme() {
 
-        // Instantiate the RequestQueue.
+        //making progress bar visible
         progressBar.visibility = View.VISIBLE
+        // Instantiate the RequestQueue for Volley
         val queue = Volley.newRequestQueue(this)
+        //this is the API url
         currentImageUrl = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, currentImageUrl,null,
             { response ->
+                //from the API JSON file, getting the "url" as it contains the image url
                 currentImageUrl = response.getString("url")
 
+                //using Glide to downlaod and convert the url to an image and output to the memeImage object on the screen
+                //added a listener object with the Glide call to turn off the progress bar when the image loads up
                 Glide.with(this).load(currentImageUrl).listener(object: RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
@@ -49,6 +56,7 @@ class MainActivity : AppCompatActivity() {
                         target: Target<Drawable>?,
                         isFirstResource: Boolean
                     ): Boolean {
+                        //progress bar becomes invisible
                         progressBar.visibility = View.GONE
                         return false
                     }
@@ -60,12 +68,14 @@ class MainActivity : AppCompatActivity() {
                         dataSource: DataSource?,
                         isFirstResource: Boolean
                     ): Boolean {
+                        //progress bar goes invisible
                         progressBar.visibility = View.GONE
                         return false
                     }
                 }).into(memeImage)
             },
             {
+                //creates a toast if there is error to call the API
                 Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
             })
 
@@ -73,14 +83,22 @@ class MainActivity : AppCompatActivity() {
         queue.add(jsonObjectRequest)
     }
 
+    //SHARE button
     fun shareMeme(view: View) {
+        //creating intent object
         val intent = Intent(Intent.ACTION_SEND)
+        //setting the type of the data that intent will send
         intent.type = "text/plain"
+        //the string that will be pushed by intent
         intent.putExtra(Intent.EXTRA_TEXT, "Epic funny meme lol $currentImageUrl")
+        //creating a chooser object to let user choose which app they want to send data through
         val chooser = Intent.createChooser(intent, "Share this meme using")
+        //starts the chooser activityu
         startActivity(chooser)
     }
+    //NEXT button
     fun nextMeme(view: View) {
+        //calls function to laod the meme
         loadMeme()
     }
 }
